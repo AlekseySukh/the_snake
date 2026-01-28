3a5cdd3caf4aec1ce012d23bc9e5e1b6897f00f9
AlekseySukh <146042801+AlekseySukh@users.noreply.github.com>
2026-01-28 11:30:14 +1000

    Delete the_snake-main directory

--- the_snake-main/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-"""Tests for main game functionality."""
-
-import pytest
-
-
-def test_snake_initialization(snake):
-    """Test snake initialization."""
-    assert len(snake.body) == 3
-    assert snake.direction == 'RIGHT'
